5b8cd2e1a535d443fac70c0d342e653f93d8f393
Florent Baudon <florent.baudon@gmail.com>
2015-05-21 10:21:29 +0200

    Ajout convas instruction

---
+++ game_jam_stunt/Assets/Menu/instructionMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class instructionMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("A1") || Input.GetButtonDown("A2") || Input.GetButtonDown("A3") || Input.GetButtonDown("A4")){
+			this.gameObject.SetActive(false);
+		}
+	}
+}
