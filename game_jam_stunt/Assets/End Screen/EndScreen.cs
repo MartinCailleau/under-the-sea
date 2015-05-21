b85ac96cdfb9d9d257d953228fd06158a54bd92c
Florent Baudon <florent.baudon@gmail.com>
2015-05-21 21:35:55 +0200

    Correction lumière level 2, bascule entre les niveaux

--- game_jam_stunt/Assets/End Screen/EndScreen.cs
+++ game_jam_stunt/Assets/End Screen/EndScreen.cs
@@ -15,7 +15,11 @@ public class EndScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("A1") || Input.GetButtonDown("A2") || Input.GetButtonDown("A3") || Input.GetButtonDown("A4")){
-			Application.LoadLevel("Proto");
+			if(Application.loadedLevelName == "Level1"){
+				Application.LoadLevel("Level2");
+			}else if (Application.loadedLevelName == "Level2"){
+				Application.LoadLevel("Level1");
+			}
 		}
 	}
 }
