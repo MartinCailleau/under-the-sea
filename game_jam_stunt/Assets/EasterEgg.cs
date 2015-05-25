0565bc76203504e969a1dd71562056a9172f86ff
Martin Cailleau <martin.cailleau@gmail.com>
2015-05-25 18:48:08 +0200

    add easter egg

---
+++ game_jam_stunt/Assets/EasterEgg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasterEgg : MonoBehaviour {
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.F) && Input.GetKeyDown(KeyCode.U) && Input.GetKeyDown(KeyCode.C) && Input.GetKeyDown(KeyCode.K)){
+			Instantiate(prefab,prefab.transform.position,prefab.transform.rotation);
+		}
+	}
+}
---
+++ game_jam_stunt/Assets/Tarbathor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tarbathor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int i = 100;
+		foreach(Rigidbody r in GetComponentsInChildren<Rigidbody> ()){
+			r.AddForce(new Vector3(0,1000,0));
+
+			r.AddTorque(new Vector3(0,50+(Random.value*100),0));
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
